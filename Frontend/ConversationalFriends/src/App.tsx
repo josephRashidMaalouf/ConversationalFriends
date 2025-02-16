76277cc8be4808a9faddeee6a67f05f95712b0e6
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-02-16 17:14:42 +0100

    .

--- Frontend/ConversationalFriends/src/App.tsx
+++ Frontend/ConversationalFriends/src/App.tsx
@@ -12,10 +12,12 @@ function App() {
   const [audioSrc, setAudioSrc] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/";
+
   const generatePodcast = async () => {
     setLoading(true); // Disable button while fetching
     try {
-      const response = await fetch("http://localhost:5000/", {
+      const response = await fetch(`${API_URL}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
